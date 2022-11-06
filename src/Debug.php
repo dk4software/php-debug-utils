e3f648cc23ed7da4596466a9c45b891c249f28fa
Dinesh Krishnan <dineshk4tryzens@gmail.com>
2022-11-06 15:10:17 +0530

    Formatted output of evaluate method

--- src/Debug.php
+++ src/Debug.php
@@ -4,9 +4,12 @@ namespace Dk4software;
 
 class Debug {
 
-    public static function evaluate($label, $var) {
-        $output = $label . json_encode($var, JSON_PRETTY_PRINT) . PHP_EOL;
-        $output = $output . '--------------------------------------------' . PHP_EOL;
-        file_put_contents('/var/www/html/var/log/mydebug.log', $output, FILE_APPEND);
+    private static $FILE_PATH = '/var/www/html/var/log/mydebug.log';
+
+    public static function evaluate($label, $var = '') {
+        $output = '#########################################' . PHP_EOL;
+        $output = $output . $label . " : " . json_encode($var, JSON_PRETTY_PRINT) . PHP_EOL;
+        $output = $output . '#########################################' . PHP_EOL;
+        file_put_contents(self::$FILE_PATH, $output, FILE_APPEND);
     }
 }
